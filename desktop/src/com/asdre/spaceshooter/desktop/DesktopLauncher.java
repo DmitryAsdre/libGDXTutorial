f0450008212984bfd3680befc0ca6c95a93cad44
DmitryMikhaylin <michalych2014@yandex.ru>
2021-07-19 20:50:58 +0300

    first lesson

---
+++ desktop/src/com/asdre/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asdre.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asdre.spaceshooter.SpaceShooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooterGame(), config);
+	}
+}
